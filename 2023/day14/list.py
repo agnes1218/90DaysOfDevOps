dce29e4c4808700efe99a1fa22343b648429e145
Ubuntu <ubuntu@ip-172-31-37-126.ap-southeast-1.compute.internal>
2023-07-31 18:02:05 +0000

    This are my task 1 & 2

---
+++ 2023/day14/list.py
@@ -0,0 +1,18 @@
+# Lists
+l = []
+ 
+# Adding Element into list
+l.append(5)
+l.append(10)
+print("Adding 5 and 10 in list", l)
+
+#tuple example
+
+colors = ('red' , 'Yellow')
+print( " Colors are as follow :", tuple(colors))
+
+#set example
+set1 = {'apple' , 'mango' , 'banana'}
+set1.add("strawberry")
+print(set1)
+
